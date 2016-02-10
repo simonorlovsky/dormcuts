41322e777326a332b0dce7b2ce6ec104a24ce0d7
simonorlovsky <simon.orlovsky@yahoo.com>
2016-02-09 22:55:04 -0600

    editing appointments

---
+++ client/templates/appointments/appointment_edit.js
@@ -0,0 +1,31 @@
+Template.appointmentEdit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var currentAppointmentId = this._id;
+
+    var appointmentProperties = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    }
+
+    Appointments.update(currentAppointmentId, {$set: appointmentProperties}, function(error) {
+      if (error) {
+        // display the error to the user
+        alert(error.reason);
+      } else {
+        Router.go('appointmentPage', {_id: currentAppointmentId});
+      }
+    });
+  },
+
+  'click .delete': function(e) {
+    e.preventDefault();
+
+    if (confirm("Delete this appointment?")) {
+      var currentAppointmentId = this._id;
+      Appointments.remove(currentAppointmentId);
+      Router.go('appointmentsList');
+    }
+  }
+});
